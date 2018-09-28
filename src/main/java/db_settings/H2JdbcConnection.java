934e33d41dcf598e275aa5d81faeed27ea6da668
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-09-28 21:24:01 +0200

    Added connection to H2 database.

---
+++ src/main/java/db_settings/H2JdbcConnection.java
@@ -0,0 +1,29 @@
+package db_settings;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class H2JdbcConnection {
+
+    static final String JDBC_DRIVER = "org.h2.Driver";
+    static final String DB_URL = "jdbc:h2:~/test";
+    static final String USER = "";
+    static final String PASSWORD = "";
+
+    public H2JdbcConnection() {
+    }
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
+        return connection;
+    }
+}
