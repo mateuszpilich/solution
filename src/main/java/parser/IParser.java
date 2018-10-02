ae0ace98083740eb23a4451e8098c7c2f4168415
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-03 00:35:51 +0200

    Corrected interface for parser.

--- src/main/java/parser/IParser.java
+++
@@ -1,9 +0,0 @@
-package parser;
-
-import domain.OrderEntity;
-
-import java.util.List;
-
-public interface IParser {
-    List<OrderEntity> readDataFromFile(String filePath, boolean removeDuplicates);
-}
