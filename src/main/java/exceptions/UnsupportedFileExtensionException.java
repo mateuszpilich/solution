cd016e1afcf4c73211f87562d995e98ee61f590b
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-03 00:41:27 +0200

    Added unsupported file extension exception.

---
+++ src/main/java/exceptions/UnsupportedFileExtensionException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class UnsupportedFileExtensionException extends Exception {
+
+    public UnsupportedFileExtensionException(String message) {
+        super(message);
+    }
+}
