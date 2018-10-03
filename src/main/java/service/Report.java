d844faff202b73e142ccfe45071bd8d22f7bcfdb
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-03 22:48:45 +0200

    Corrected report service.

--- src/main/java/service/Report.java
+++ src/main/java/service/Report.java
@@ -1,7 +1,7 @@
 package service;
 
 import dao.DaoRepository;
-import domain.OrderEntity;
+import domain.Request;
 import org.apache.log4j.Logger;
 
 import java.sql.SQLException;
@@ -13,97 +13,97 @@ import java.util.List;
 public class Report {
 
     private DaoRepository daoRepository = new DaoRepository();
-    private static Logger logger = Logger.getLogger(Report.class);
+    private static final Logger LOGGER = Logger.getLogger(Report.class);
 
     /**
-     * The method counts the total number of orders.
+     * The method counts the total number of requests.
      *
      * @throws SQLException
      */
-    public void totalOrdersNumber() throws SQLException {
-        logger.debug("Number of total orders: " + daoRepository.totalOrdersNumber());
+    public void totalRequestsNumber() throws SQLException {
+        LOGGER.debug("Number of total requests: " + daoRepository.totalRequestsNumber());
     }
 
     /**
-     * The method counts the total number of orders for the customer with the indicated id.
+     * The method counts the total number of requests for the customer with the indicated id.
      *
      * @param clientId
      * @throws SQLException
      */
-    public void totalOrdersNumberToClientById(Long clientId) throws SQLException {
-        logger.debug("Number of total orders to client for id: " + clientId + " is: " + daoRepository.totalOrdersNumberByClient(clientId));
+    public void totalRequestsNumberToClientById(Long clientId) throws SQLException {
+        LOGGER.debug("Number of total requests to client for id: " + clientId + " is: " + daoRepository.totalRequestsNumberByClient(clientId));
     }
 
     /**
-     * The method counts the total amount of orders.
+     * The method counts the total amount of requests.
      *
      * @throws SQLException
      */
-    public void totalOrdersPrice() throws SQLException {
-        logger.debug("Price of total orders: " + daoRepository.totalOrdersPrice());
+    public void totalRequestsPrice() throws SQLException {
+        LOGGER.debug("Price of total requests: " + daoRepository.totalRequestsPrice());
     }
 
     /**
-     * The method counts the total amount of orders for the customer with the indicated id.
-     *
-     * @param clientId
-     * @throws SQLException
-     */
-    public void totalOrdersPriceToClientById(Long clientId) throws SQLException {
-        logger.debug("Price of total orders to client for id: " + clientId + " is: " + daoRepository.totalOrdersPriceByClient(clientId));
-    }
-
-    /**
-     * The method shows a list of all orders.
-     *
-     * @throws SQLException
-     */
-    public void listOfAllOrders() throws SQLException {
-        List<OrderEntity> orderEntities = daoRepository.listOfAllOrders();
-        logger.debug("List of total orders:");
-        orderEntities.forEach(order ->
-                logger.debug(order.getId() + ","
-                        + order.getClientId() + ","
-                        + order.getRequestId() + ","
-                        + order.getName() + ","
-                        + order.getQuantity() + ","
-                        + order.getPrice()));
-    }
-
-    /**
-     * The method shows a list of all orders for the customer with the indicated id.
+     * The method counts the total amount of requests for the customer with the indicated id.
      *
      * @param clientId
      * @throws SQLException
      */
-    public void listOfAllOrdersToClientById(Long clientId) throws SQLException {
-        List<OrderEntity> orderEntities = daoRepository.listOfAllOrdersToClientById(clientId);
-        logger.debug("List of total orders to client for id: " + clientId + " is:");
-        orderEntities.forEach(order ->
-                logger.debug(order.getId() + ","
-                        + order.getClientId() + ","
-                        + order.getRequestId() + ","
-                        + order.getName() + ","
-                        + order.getQuantity() + ","
-                        + order.getPrice()));
+    public void totalRequestsPriceToClientById(Long clientId) throws SQLException {
+        LOGGER.debug("Price of total requests to client for id: " + clientId + " is: " + daoRepository.totalRequestsPriceByClient(clientId));
     }
 
     /**
-     * The method counts average value of order.
+     * The method shows a list of all requests.
      *
      * @throws SQLException
      */
-    public void averageValueOfOrder() throws SQLException {
-        logger.debug("Average value of order: " + daoRepository.averageValueOfOrder());
+    public void listOfAllRequests() throws SQLException {
+        List<Request> requestsEntities = daoRepository.listOfAllRequests();
+        LOGGER.debug("List of total requests:");
+        requestsEntities.forEach(request ->
+                LOGGER.debug(request.getId() + ","
+                        + request.getClientId() + ","
+                        + request.getRequestId() + ","
+                        + request.getName() + ","
+                        + request.getQuantity() + ","
+                        + request.getPrice()));
     }
 
     /**
-     * The method counts the average value of order for the customer with the indicated id.
+     * The method shows a list of all requests for the customer with the indicated id.
      *
      * @param clientId
      * @throws SQLException
      */
-    public void averageValueOfOrderToClientById(Long clientId) throws SQLException {
-        logger.debug("Average value of order to client for id: " + clientId + " is: " + daoRepository.averageValueOfOrderToClientById(clientId));
+    public void listOfAllRequestsToClientById(Long clientId) throws SQLException {
+        List<Request> requestsEntities = daoRepository.listOfAllRequestsToClientById(clientId);
+        LOGGER.debug("List of total requests to client for id: " + clientId + " is:");
+        requestsEntities.forEach(request ->
+                LOGGER.debug(request.getId() + ","
+                        + request.getClientId() + ","
+                        + request.getRequestId() + ","
+                        + request.getName() + ","
+                        + request.getQuantity() + ","
+                        + request.getPrice()));
+    }
+
+    /**
+     * The method counts average value of request.
+     *
+     * @throws SQLException
+     */
+    public void averageValueOfRequest() throws SQLException {
+        LOGGER.debug("Average value of request: " + daoRepository.averageValueOfRequest());
+    }
+
+    /**
+     * The method counts the average value of request for the customer with the indicated id.
+     *
+     * @param clientId
+     * @throws SQLException
+     */
+    public void averageValueOfRequestToClientById(Long clientId) throws SQLException {
+        LOGGER.debug("Average value of request to client for id: " + clientId + " is: " + daoRepository.averageValueOfRequestToClientById(clientId));
     }
 }
