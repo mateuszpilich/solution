c3f6624755c7e121a5c54f41587b2890d77924c7
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-03 22:22:36 +0200

    Corrected parser interface.

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -1,6 +1,6 @@
 package parser;
 
-import domain.OrderEntity;
+import domain.Request;
 import exceptions.UnsupportedFileExtensionException;
 
 import java.util.List;
@@ -9,6 +9,7 @@ import java.util.List;
  * The class have a set of functionalities for the ParserImpl class.
  */
 public interface Parser {
-    List<OrderEntity> readDataFromFile(String filePath, boolean removeDuplicates) throws UnsupportedFileExtensionException;
-    List<OrderEntity> readDataFromFile(String[] filesPaths, boolean removeDuplicates) throws UnsupportedFileExtensionException;
+    List<Request> readDataFromFile(String filePath, boolean removeDuplicates) throws UnsupportedFileExtensionException;
+
+    List<Request> readDataFromFile(List<String> filesPaths, boolean removeDuplicates) throws UnsupportedFileExtensionException;
 }
