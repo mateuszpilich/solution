2856c5f81a549e6abfd73043af8495c99ed5b5d1
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-03 22:47:05 +0200

    Corrected h2 jdbc connection.

--- src/main/java/settings/H2JdbcConnection.java
+++ src/main/java/settings/H2JdbcConnection.java
@@ -1,5 +1,8 @@
 package settings;
 
+import org.apache.log4j.Logger;
+import service.Report;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,14 +11,11 @@ import java.sql.SQLException;
  * The class helps connection from database.
  */
 public class H2JdbcConnection {
-
-    static final String JDBC_DRIVER = "org.h2.Driver";
-    static final String DB_URL = "jdbc:h2:./Database/my";
-    static final String USER = "root";
-    static final String PASSWORD = "mypassword";
-
-    public H2JdbcConnection() {
-    }
+    private static final String JDBC_DRIVER = "org.h2.Driver";
+    private static final String DB_URL = "jdbc:h2:mem:dbInMemory;INIT=runscript from 'classpath:createTable.sql'";
+    private static final String USER = "root";
+    private static final String PASSWORD = "mypassword";
+    private static final Logger LOGGER = Logger.getLogger(Report.class);
 
     public Connection getConnection() {
         Connection connection = null;
@@ -23,7 +23,9 @@ public class H2JdbcConnection {
             Class.forName(JDBC_DRIVER);
             connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
         } catch (ClassNotFoundException e) {
+            LOGGER.error(e.getMessage());
         } catch (SQLException se) {
+            LOGGER.error(se.getMessage());
         }
         return connection;
     }
