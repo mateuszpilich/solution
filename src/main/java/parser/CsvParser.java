1c07d5c5eae25da6f942cce37de1c540a29f2e1f
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-04 18:09:26 +0200

    Corrected parser interface.

--- src/main/java/parser/CsvParser.java
+++ src/main/java/parser/CsvParser.java
@@ -1,3 +1,8 @@
+/**
+ * Parser contain tools for parse data files to POJO.
+ *
+ * @version 1.0
+ */
 package parser;
 
 import domain.Request;
@@ -20,20 +25,64 @@ import java.util.stream.Stream;
  * The class helps read data from files csv.
  */
 public class CsvParser {
+    /**
+     * New request.
+     */
     private Request request;
+
+    /**
+     * This is number of index for client id.
+     */
+    private static final int INDEX_FOR_CLIENT_ID = 0;
+
+    /**
+     * This is number of index for request id.
+     */
+    private static final int INDEX_FOR_REQUEST_ID = 1;
+
+    /**
+     * This is number of index for name.
+     */
+    private static final int INDEX_FOR_NAME = 2;
+
+    /**
+     * This is number of index for quantity.
+     */
+    private static final int INDEX_FOR_QUANTITY = 3;
+
+    /**
+     * This is number of index for price.
+     */
+    private static final int INDEX_FOR_PRICE = 4;
+
+    /**
+     * This is the list of requests.
+     */
     private List<Request> requestsEntities = new ArrayList<Request>();
-    private Set<Request> requestsEntitiesWithoutDuplicates = new HashSet<Request>();
+
+    /**
+     * This is the set of requests.
+     */
+    private Set<Request> requestsEntitiesWithoutDuplicates =
+            new HashSet<Request>();
+
+    /**
+     * This is logger.
+     */
     private static final Logger LOGGER = Logger.getLogger(ReportImpl.class);
 
     /**
      * Method to parse content of file given by path.
      *
      * @param filePath         is path to file
-     * @param removeDuplicates true if duplicate requests are to be removed, otherwise false
+     * @param removeDuplicates true if duplicate requests are to be removed,
+     *                         otherwise false
      * @return list with objects extracted from file, otherwise throws exception
      */
-    public List<Request> readRequests(String filePath, boolean removeDuplicates) {
-        try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
+    public final List<Request> readRequests(final String filePath,
+                                            final boolean removeDuplicates) {
+        try (Stream<String> stream = Files.lines(Paths.get(filePath),
+                StandardCharsets.UTF_8)) {
             stream.forEach(request -> {
                 try {
                     requestsEntities.add(createRequest(request));
@@ -48,7 +97,8 @@ public class CsvParser {
         }
 
         if (removeDuplicates) {
-            requestsEntitiesWithoutDuplicates = new HashSet<Request>(requestsEntities);
+            requestsEntitiesWithoutDuplicates =
+                    new HashSet<Request>(requestsEntities);
             requestsEntities.clear();
             requestsEntities.addAll(requestsEntitiesWithoutDuplicates);
         }
@@ -62,17 +112,19 @@ public class CsvParser {
      * @return new request entity
      * @throws WrongInputDataException incorrect request data
      */
-    private Request createRequest(String requestFromFile) throws WrongInputDataException {
+    private Request createRequest(final String requestFromFile) throws WrongInputDataException {
         String[] request = requestFromFile.split(",");
         Request requestEntity;
         try {
             Long id = null;
-            String clientId = request[0];
-            Long requestId = Long.parseLong(request[1]);
-            String name = request[2];
-            Integer quantity = Integer.parseInt(request[3]);
-            BigDecimal price = BigDecimal.valueOf(Double.parseDouble(request[4]));
-            requestEntity = new Request(id, clientId, requestId, name, quantity, price);
+            String clientId = request[INDEX_FOR_CLIENT_ID];
+            Long requestId = Long.parseLong(request[INDEX_FOR_REQUEST_ID]);
+            String name = request[INDEX_FOR_NAME];
+            Integer quantity = Integer.parseInt(request[INDEX_FOR_QUANTITY]);
+            BigDecimal price =
+                    BigDecimal.valueOf(Double.parseDouble(request[INDEX_FOR_PRICE]));
+            requestEntity = new Request(id, clientId, requestId, name,
+                    quantity, price);
         } catch (Exception e) {
             LOGGER.error("Incorrect request data!");
             throw new WrongInputDataException("Incorrect request data!");
