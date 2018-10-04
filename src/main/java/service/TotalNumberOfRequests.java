56d9cb2ead2130815caac41adbba3510124352c4
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-04 11:13:15 +0200

    Added object total number of requests for serialization to xml file.

---
+++ src/main/java/service/TotalNumberOfRequests.java
@@ -0,0 +1,17 @@
+package service;
+
+public class TotalNumberOfRequests {
+    private Long amount;
+
+    public TotalNumberOfRequests(Long amount) {
+        this.amount = amount;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+}
