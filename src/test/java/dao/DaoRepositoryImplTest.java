a414c26d71c283b8e126f38ae4fba4c894944ed2
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-09 15:51:09 +0200

    Corrected dao repository impl test.

--- src/test/java/dao/DaoRepositoryImplTest.java
+++ src/test/java/dao/DaoRepositoryImplTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -31,5 +32,186 @@ public class DaoRepositoryImplTest {
 
         // then
         assertEquals(Long.valueOf(14), result);
+    }
+
+    @Test
+    public void totalRequestsNumber_2() throws SQLException {
+        // given
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+        Mockito.doReturn(true).when(resultSet).next();
+        Mockito.doReturn(Long.valueOf(0)).when(resultSet).getLong(Mockito.anyInt());
+
+        // when
+        Mockito.doReturn(resultSet).when(this.daoRepository).executeQuery(Mockito.anyString());
+        Long result = daoRepository.totalRequestsNumber();
+
+        // then
+        assertEquals(Long.valueOf(0), result);
+    }
+
+    @Test
+    public void totalRequestsNumberByClientId_1() throws SQLException {
+        // given
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+        Mockito.doReturn(true).when(resultSet).next();
+        Mockito.doReturn(Long.valueOf(4)).when(resultSet).getLong(Mockito.anyInt());
+
+        // when
+        Mockito.doReturn(resultSet).when(this.daoRepository).executeQuery(Mockito.anyString());
+        Long result = daoRepository.totalRequestsNumberByClientId(1L);
+
+        // then
+        assertEquals(Long.valueOf(4), result);
+    }
+
+    @Test
+    public void totalRequestsNumberByClientId_2() throws SQLException {
+        // given
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+        Mockito.doReturn(true).when(resultSet).next();
+        Mockito.doReturn(Long.valueOf(0)).when(resultSet).getLong(Mockito.anyInt());
+
+        // when
+        Mockito.doReturn(resultSet).when(this.daoRepository).executeQuery(Mockito.anyString());
+        Long result = daoRepository.totalRequestsNumberByClientId(1L);
+
+        // then
+        assertEquals(Long.valueOf(0), result);
+    }
+
+    @Test
+    public void totalRequestsPrice_1() throws SQLException {
+        // given
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+        Mockito.doReturn(true).when(resultSet).next();
+        Mockito.doReturn(BigDecimal.valueOf(120.0).setScale(2,
+                BigDecimal.ROUND_CEILING)).when(resultSet).getBigDecimal(Mockito.anyInt());
+
+        // when
+        Mockito.doReturn(resultSet).when(this.daoRepository).executeQuery(Mockito.anyString());
+        BigDecimal result = daoRepository.totalRequestsPrice();
+
+        // then
+        assertEquals(BigDecimal.valueOf(120.0).setScale(2,
+                BigDecimal.ROUND_CEILING), result);
+    }
+
+    @Test
+    public void totalRequestsPrice_2() throws SQLException {
+        // given
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+        Mockito.doReturn(true).when(resultSet).next();
+        Mockito.doReturn(BigDecimal.valueOf(0.0).setScale(2,
+                BigDecimal.ROUND_CEILING)).when(resultSet).getBigDecimal(Mockito.anyInt());
+
+        // when
+        Mockito.doReturn(resultSet).when(this.daoRepository).executeQuery(Mockito.anyString());
+        BigDecimal result = daoRepository.totalRequestsPrice();
+
+        // then
+        Mockito.doReturn(BigDecimal.valueOf(0.0).setScale(2,
+                BigDecimal.ROUND_CEILING)).when(resultSet).getBigDecimal(Mockito.anyInt());
+    }
+
+    @Test
+    public void totalRequestsPriceByClientId_1() throws SQLException {
+        // given
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+        Mockito.doReturn(true).when(resultSet).next();
+        Mockito.doReturn(BigDecimal.valueOf(20.0).setScale(2,
+                BigDecimal.ROUND_CEILING)).when(resultSet).getBigDecimal(Mockito.anyInt());
+
+        // when
+        Mockito.doReturn(resultSet).when(this.daoRepository).executeQuery(Mockito.anyString());
+        BigDecimal result = daoRepository.totalRequestsPriceByClientId(1L);
+
+        // then
+        assertEquals(BigDecimal.valueOf(20.0).setScale(2,
+                BigDecimal.ROUND_CEILING), result);
+    }
+
+    @Test
+    public void totalRequestsPriceByClientId_2() throws SQLException {
+        // given
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+        Mockito.doReturn(true).when(resultSet).next();
+        Mockito.doReturn(BigDecimal.valueOf(0.0).setScale(2,
+                BigDecimal.ROUND_CEILING)).when(resultSet).getBigDecimal(Mockito.anyInt());
+
+        // when
+        Mockito.doReturn(resultSet).when(this.daoRepository).executeQuery(Mockito.anyString());
+        BigDecimal result = daoRepository.totalRequestsPriceByClientId(1L);
+
+        // then
+        Mockito.doReturn(BigDecimal.valueOf(0.0).setScale(2,
+                BigDecimal.ROUND_CEILING)).when(resultSet).getBigDecimal(Mockito.anyInt());
+    }
+
+    @Test
+    public void averageValueOfRequest_1() throws SQLException {
+        // given
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+        Mockito.doReturn(true).when(resultSet).next();
+        Mockito.doReturn(BigDecimal.valueOf(13.33).setScale(2,
+                BigDecimal.ROUND_CEILING)).when(resultSet).getBigDecimal(Mockito.anyInt());
+
+        // when
+        Mockito.doReturn(resultSet).when(this.daoRepository).executeQuery(Mockito.anyString());
+        BigDecimal result = daoRepository.averageValueOfRequest();
+
+        // then
+        assertEquals(BigDecimal.valueOf(13.33).setScale(2,
+                BigDecimal.ROUND_CEILING), result);
+    }
+
+    @Test
+    public void averageValueOfRequest_2() throws SQLException {
+        // given
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+        Mockito.doReturn(true).when(resultSet).next();
+        Mockito.doReturn(BigDecimal.valueOf(0.0).setScale(2,
+                BigDecimal.ROUND_CEILING)).when(resultSet).getBigDecimal(Mockito.anyInt());
+
+        // when
+        Mockito.doReturn(resultSet).when(this.daoRepository).executeQuery(Mockito.anyString());
+        BigDecimal result = daoRepository.averageValueOfRequest();
+
+        // then
+        Mockito.doReturn(BigDecimal.valueOf(0.0).setScale(2,
+                BigDecimal.ROUND_CEILING)).when(resultSet).getBigDecimal(Mockito.anyInt());
+    }
+
+    @Test
+    public void averageValueOfRequestToClientById_1() throws SQLException {
+        // given
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+        Mockito.doReturn(true).when(resultSet).next();
+        Mockito.doReturn(BigDecimal.valueOf(11.67).setScale(2,
+                BigDecimal.ROUND_CEILING)).when(resultSet).getBigDecimal(Mockito.anyInt());
+
+        // when
+        Mockito.doReturn(resultSet).when(this.daoRepository).executeQuery(Mockito.anyString());
+        BigDecimal result = daoRepository.averageValueOfRequestToClientById(1L);
+
+        // then
+        assertEquals(BigDecimal.valueOf(11.67).setScale(2,
+                BigDecimal.ROUND_CEILING), result);
+    }
+
+    @Test
+    public void averageValueOfRequestToClientById_2() throws SQLException {
+        // given
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+        Mockito.doReturn(true).when(resultSet).next();
+        Mockito.doReturn(BigDecimal.valueOf(0.0).setScale(2,
+                BigDecimal.ROUND_CEILING)).when(resultSet).getBigDecimal(Mockito.anyInt());
+
+        // when
+        Mockito.doReturn(resultSet).when(this.daoRepository).executeQuery(Mockito.anyString());
+        BigDecimal result = daoRepository.averageValueOfRequestToClientById(1L);
+
+        // then
+        Mockito.doReturn(BigDecimal.valueOf(0.0).setScale(2,
+                BigDecimal.ROUND_CEILING)).when(resultSet).getBigDecimal(Mockito.anyInt());
     }
 }
