7111f347b475ea94bb816f8078413cf0ac6e8907
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-10 22:50:17 +0200

    Corrected average value of request.

--- src/main/java/service/AverageValueOfRequest.java
+++ src/main/java/service/AverageValueOfRequest.java
@@ -2,18 +2,40 @@ package service;
 
 import java.math.BigDecimal;
 
+/**
+ * The class helps represents reports in xml for average value of request.
+ */
 public class AverageValueOfRequest {
+
+    /**
+     * This is value for average value of request.
+     */
     private BigDecimal value;
 
-    public AverageValueOfRequest(BigDecimal value) {
-        this.value = value;
+    /**
+     * This is constructor for average rValue of request.
+     *
+     * @param rValue is rValue for average rValue of request
+     */
+    public AverageValueOfRequest(final BigDecimal rValue) {
+        this.value = rValue;
     }
 
-    public BigDecimal getValue() {
+    /**
+     * This is getter for rValue for average rValue of request.
+     *
+     * @return is value for average of request
+     */
+    public final BigDecimal getValue() {
         return value;
     }
 
-    public void setValue(BigDecimal value) {
-        this.value = value;
+    /**
+     * This is setter for rValue for average rValue of request.
+     *
+     * @param rValue is rValue for average rValue of request
+     */
+    public final void setValue(final BigDecimal rValue) {
+        this.value = rValue;
     }
 }
