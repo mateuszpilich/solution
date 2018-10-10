a462b6fc1f856036626e89dc7a21fa93ff0f72f2
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-11 00:24:46 +0200

    Corrected total requests number to client by id.

--- src/main/java/service/TotalRequestsNumberToClientById.java
+++ src/main/java/service/TotalRequestsNumberToClientById.java
@@ -1,27 +1,65 @@
 package service;
 
+/**
+ * This class helps represent reports in xml file for total requests number
+ * to client by id.
+ */
 public class TotalRequestsNumberToClientById {
+    /**
+     * This is client id for total requests number to client by id.
+     */
     private Long clientId;
+
+    /**
+     * This is amount for total requests number to client by id.
+     */
     private Long amount;
 
-    public TotalRequestsNumberToClientById(Long clientId, Long amount) {
-        this.clientId = clientId;
-        this.amount = amount;
+    /**
+     * This is constructor for total requests number to client by id.
+     *
+     * @param rClientId is client id for total requests number to client by id
+     * @param rAmount   is rAmount for total requests number to client by id
+     */
+    public TotalRequestsNumberToClientById(final Long rClientId,
+                                           final Long rAmount) {
+        this.clientId = rClientId;
+        this.amount = rAmount;
     }
 
-    public Long getClientId() {
+    /**
+     * This is getter for client id for total requests number to client by id.
+     *
+     * @return is client id for total requests number to client by id
+     */
+    public final Long getClientId() {
         return clientId;
     }
 
-    public void setClientId(Long clientId) {
-        this.clientId = clientId;
+    /**
+     * This is setter for client id for total requests number to client by id.
+     *
+     * @param rClientId is client id for total requests number to client by id
+     */
+    public final void setClientId(final Long rClientId) {
+        this.clientId = rClientId;
     }
 
-    public Long getAmount() {
+    /**
+     * This is getter for amount for total requests number to client by id.
+     *
+     * @return is amount for total requests number to client by id
+     */
+    public final Long getAmount() {
         return amount;
     }
 
-    public void setAmount(Long amount) {
-        this.amount = amount;
+    /**
+     * This is setter for rAmount for total requests number to client by id.
+     *
+     * @param rAmount is rAmount for total requests number to client by id
+     */
+    public final void setAmount(final Long rAmount) {
+        this.amount = rAmount;
     }
 }
