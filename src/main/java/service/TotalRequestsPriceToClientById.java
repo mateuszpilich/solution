b5aa8eeca9cd795947225be33a4f13b73738bd4a
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-11 00:35:53 +0200

    Corrected total requests price to client by id.

--- src/main/java/service/TotalRequestsPriceToClientById.java
+++ src/main/java/service/TotalRequestsPriceToClientById.java
@@ -2,28 +2,66 @@ package service;
 
 import java.math.BigDecimal;
 
+/**
+ * The class helps represents reports in xml file for total requests price to
+ * client by id.
+ */
 public class TotalRequestsPriceToClientById {
+    /**
+     * This is client id for total requests price to client by id.
+     */
     private Long clientId;
+
+    /**
+     * This is price for total requests price to client by id.
+     */
     private BigDecimal price;
 
-    public TotalRequestsPriceToClientById(Long clientId, BigDecimal price) {
-        this.clientId = clientId;
-        this.price = price;
+    /**
+     * This is constructor for total requests rPrice to client by id.
+     *
+     * @param rClientId is client id for total requests rPrice to client by id
+     * @param rPrice    is rPrice for total requests rPrice to client by id
+     */
+    public TotalRequestsPriceToClientById(final Long rClientId,
+                                          final BigDecimal rPrice) {
+        this.clientId = rClientId;
+        this.price = rPrice;
     }
 
-    public Long getClientId() {
+    /**
+     * This is getter for total requests rPrice to client by id.
+     *
+     * @return is client id for total requests rPrice to client by id
+     */
+    public final Long getClientId() {
         return clientId;
     }
 
-    public void setClientId(Long clientId) {
-        this.clientId = clientId;
+    /**
+     * This is setter for total requests rPrice to client by id.
+     *
+     * @param rClientId is client id for total requests rPrice to client by id
+     */
+    public final void setClientId(final Long rClientId) {
+        this.clientId = rClientId;
     }
 
-    public BigDecimal getPrice() {
+    /**
+     * This is getter for price for total requests rPrice to client by id.
+     *
+     * @return is price for total requests rPrice to client by id
+     */
+    public final BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(BigDecimal price) {
-        this.price = price;
+    /**
+     * This is setter for rPrice for total requests rPrice to client by id.
+     *
+     * @param rPrice is rPrice for total requests rPrice to client by id
+     */
+    public final void setPrice(final BigDecimal rPrice) {
+        this.price = rPrice;
     }
 }
