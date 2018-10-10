3623d281c407cb7769da6e454733452e4a9e3c90
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-10 22:20:20 +0200

    Corrected unsupported file extension exception.

--- src/main/java/exceptions/UnsupportedFileExtensionException.java
+++ src/main/java/exceptions/UnsupportedFileExtensionException.java
@@ -1,8 +1,15 @@
 package exceptions;
 
+/**
+ * The class helps user understand that extension of this file is unsupported.
+ */
 public class UnsupportedFileExtensionException extends Exception {
 
-    public UnsupportedFileExtensionException(String message) {
+    /**
+     * This is constructor unsupported file extension exception.
+     * @param message is message for user
+     */
+    public UnsupportedFileExtensionException(final String message) {
         super(message);
     }
 }
