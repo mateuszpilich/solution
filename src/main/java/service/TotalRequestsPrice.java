a24b2beebd788ca330466dd994941c4bcb435e51
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-11 00:29:33 +0200

    Corrected total requests price.

--- src/main/java/service/TotalRequestsPrice.java
+++ src/main/java/service/TotalRequestsPrice.java
@@ -2,18 +2,39 @@ package service;
 
 import java.math.BigDecimal;
 
+/**
+ * The class helps represents reports in xml file for total requests price.
+ */
 public class TotalRequestsPrice {
+    /**
+     * This is price for total requests price.
+     */
     private BigDecimal price;
 
-    public TotalRequestsPrice(BigDecimal price) {
-        this.price = price;
+    /**
+     * This is constructor for total requests rPrice.
+     *
+     * @param rPrice is rPrice for total requests rPrice
+     */
+    public TotalRequestsPrice(final BigDecimal rPrice) {
+        this.price = rPrice;
     }
 
-    public BigDecimal getPrice() {
+    /**
+     * This is getter for price for total requests price.
+     *
+     * @return is price for total requests price
+     */
+    public final BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(BigDecimal price) {
-        this.price = price;
+    /**
+     * This is setter for rPrice for total requests rPrice.
+     *
+     * @param rPrice is rPrice for total requests rPrice
+     */
+    public final void setPrice(final BigDecimal rPrice) {
+        this.price = rPrice;
     }
 }
