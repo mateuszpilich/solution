de1d0af6136828b9297c9883c08179a2b7feb9cf
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-11 00:45:58 +0200

    Corrected writer interface.

--- src/main/java/writer/Writer.java
+++ src/main/java/writer/Writer.java
@@ -1,8 +1,14 @@
 package writer;
 
 /**
- * The class have a set of functionalities for the WriterImpl class.
+ * This interface have a set of functionalities for the WriterImpl class.
  */
 public interface Writer {
+    /**
+     * Method to write reports to files.
+     *
+     * @param xmlFileName is name of xml file for report
+     * @param reportClass is type of report class
+     */
     void writeReportsToFile(String xmlFileName, Object reportClass);
 }
