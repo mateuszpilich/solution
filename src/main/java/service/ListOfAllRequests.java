efc78f1353982f50f2ba965213f3fc64b45b79cb
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-10 23:50:37 +0200

    Corrected list of all requests.

--- src/main/java/service/ListOfAllRequests.java
+++ src/main/java/service/ListOfAllRequests.java
@@ -4,18 +4,41 @@ import domain.Request;
 
 import java.util.List;
 
+/**
+ * The class helps represents reports in xml file for list of all requests.
+ */
 public class ListOfAllRequests {
+
+    /**
+     * This is list of all requests.
+     */
     private List<Request> listOfAllRequests;
 
-    public ListOfAllRequests(List<Request> listOfAllRequests) {
-        this.listOfAllRequests = listOfAllRequests;
+    /**
+     * This is constructor for list of all requests.
+     *
+     * @param rListOfAllRequests is list of all requests
+     */
+    public ListOfAllRequests(final List<Request> rListOfAllRequests) {
+        this.listOfAllRequests = rListOfAllRequests;
     }
 
-    public List<Request> getListOfAllRequests() {
+    /**
+     * This is getter for list of all requests.
+     *
+     * @return is list of all requests
+     */
+    public final List<Request> getListOfAllRequests() {
         return listOfAllRequests;
     }
 
-    public void setListOfAllRequests(List<Request> listOfAllRequests) {
-        this.listOfAllRequests = listOfAllRequests;
+    /**
+     * This is setter for list of all requests.
+     *
+     * @param rListOfAllRequests is list of all requests
+     */
+    public final void setListOfAllRequests(
+            final List<Request> rListOfAllRequests) {
+        this.listOfAllRequests = rListOfAllRequests;
     }
 }
