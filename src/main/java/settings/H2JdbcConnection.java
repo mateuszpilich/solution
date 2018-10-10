771c9cbbd185d80c094a4f92ccff226923133ff9
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-11 00:42:38 +0200

    Corrected h2 jdbc connection.

--- src/main/java/settings/H2JdbcConnection.java
+++ src/main/java/settings/H2JdbcConnection.java
@@ -11,13 +11,38 @@ import java.sql.SQLException;
  * The class helps connection from database.
  */
 public class H2JdbcConnection {
+    /**
+     * This is jdbc driver for h2.
+     */
     private static final String JDBC_DRIVER = "org.h2.Driver";
-    private static final String DB_URL = "jdbc:h2:mem:dbInMemory;INIT=runscript from 'classpath:createTable.sql'";
+
+    /**
+     * This is url for database.
+     */
+    private static final String DB_URL = "jdbc:h2:mem:dbInMemory;"
+            + "INIT=runscript from 'classpath:createTable.sql'";
+
+    /**
+     * This is name of user for connect to database.
+     */
     private static final String USER = "root";
+
+    /**
+     * This is password for user for connect to database.
+     */
     private static final String PASSWORD = "mypassword";
+
+    /**
+     * This is logger.
+     */
     private static final Logger LOGGER = Logger.getLogger(ReportImpl.class);
 
-    public Connection getConnection() {
+    /**
+     * This is session with database.
+     *
+     * @return is session with database
+     */
+    public final Connection getConnection() {
         Connection connection = null;
         try {
             Class.forName(JDBC_DRIVER);
