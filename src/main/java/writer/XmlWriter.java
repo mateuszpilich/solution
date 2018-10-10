879e536844af913f80cec9ba5a09c14615530447
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-11 00:50:34 +0200

    Corrected xml writer.

--- src/main/java/writer/XmlWriter.java
+++ src/main/java/writer/XmlWriter.java
@@ -11,17 +11,34 @@ import java.io.IOException;
  * The class helps save reports to file xml.
  */
 public class XmlWriter {
+    /**
+     * This is xmp mapper.
+     */
     private XmlMapper xmlMapper;
+
+    /**
+     * This is logger.
+     */
     private static final Logger LOGGER = Logger.getLogger(ReportImpl.class);
 
+    /**
+     * This is constructor for xml writer.
+     */
     public XmlWriter() {
         this.xmlMapper = new XmlMapper();
     }
 
-    public void writeReportsToXmlFile(String xmlFileName, Object reportClass) {
+    /**
+     * Method to write reports to xml file.
+     *
+     * @param xmlFileName is name of xml file for report
+     * @param reportClass is type of report class
+     */
+    public final void writeReportsToXmlFile(final String xmlFileName,
+                                            final Object reportClass) {
         try {
-            xmlMapper.writeValue(new File("report_" + xmlFileName + ".xml"), reportClass);
-            File file = new File(xmlFileName);
+            xmlMapper.writeValue(new File("report_" + xmlFileName + ".xml"),
+                    reportClass);
         } catch (IOException e) {
             LOGGER.error(e.getMessage());
         }
