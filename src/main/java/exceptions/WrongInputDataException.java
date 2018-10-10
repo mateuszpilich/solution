57b8610ee8abcbcb533be7bca738570f2d49ad6e
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-10 22:20:47 +0200

    Corrected wrong input data exception.

--- src/main/java/exceptions/WrongInputDataException.java
+++ src/main/java/exceptions/WrongInputDataException.java
@@ -1,8 +1,16 @@
 package exceptions;
 
+/**
+ * The class helps user understand that data in this file is incorrect.
+ */
 public class WrongInputDataException extends Exception {
 
-    public WrongInputDataException(String message) {
+    /**
+     * This is constructor wrong input data exception.
+     *
+     * @param message is message for user
+     */
+    public WrongInputDataException(final String message) {
         super(message);
     }
 }
