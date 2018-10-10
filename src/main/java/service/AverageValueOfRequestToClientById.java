a2c2f0e6c4d6358ee53b3107f9ca1ca3b4a255fc
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-10 23:00:51 +0200

    Corrected average value of request to client by id.

--- src/main/java/service/AverageValueOfRequestToClientById.java
+++ src/main/java/service/AverageValueOfRequestToClientById.java
@@ -2,28 +2,73 @@ package service;
 
 import java.math.BigDecimal;
 
+/**
+ * The class helps represents reports in xml for average value of request to
+ * client by id.
+ */
 public class AverageValueOfRequestToClientById {
+
+    /**
+     * This is client id for average value of request to client by id.
+     */
     private Long clientId;
+
+    /**
+     * This is value for average value of request to client by id.
+     */
     private BigDecimal value;
 
-    public AverageValueOfRequestToClientById(Long clientId, BigDecimal value) {
-        this.clientId = clientId;
-        this.value = value;
+    /**
+     * This is constructor for average rValue of request to client by id.
+     *
+     * @param rClientId is client id for average rValue of request to client by
+     *                  id
+     * @param rValue    is rValue for average rValue of request to client by id
+     */
+    public AverageValueOfRequestToClientById(final Long rClientId,
+                                             final BigDecimal rValue) {
+        this.clientId = rClientId;
+        this.value = rValue;
     }
 
-    public Long getClientId() {
+    /**
+     * This is getter for client id for average value of request to client by
+     * id.
+     *
+     * @return is client id for average value of request to client by id
+     */
+    public final Long getClientId() {
         return clientId;
     }
 
-    public void setClientId(Long clientId) {
-        this.clientId = clientId;
+    /**
+     * This is setter for client id for average value of request to client by
+     * id.
+     *
+     * @param rClientId is client id for average value of request to client by
+     *                  id
+     */
+    public final void setClientId(final Long rClientId) {
+        this.clientId = rClientId;
     }
 
-    public BigDecimal getValue() {
+    /**
+     * This is getter for value for average value of request to client by
+     * id.
+     *
+     * @return is value for average value of request to client by id
+     */
+    public final BigDecimal getValue() {
         return value;
     }
 
-    public void setValue(BigDecimal value) {
-        this.value = value;
+    /**
+     * This is setter for rValue for average rValue of request to client by
+     * id.
+     *
+     * @param rValue is rValue for average rValue of request to client by id
+     */
+    public final void setValue(final BigDecimal rValue) {
+        this.value = rValue;
     }
 }
