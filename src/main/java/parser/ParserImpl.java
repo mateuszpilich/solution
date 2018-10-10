4740e868dd5b29529f3d175459e7eebba6741571
Mateusz Pilich <mateusz.pilich@gmail.com>
2018-10-10 23:02:05 +0200

    Corrected parser impl.

--- src/main/java/parser/ParserImpl.java
+++ src/main/java/parser/ParserImpl.java
@@ -13,10 +13,25 @@ import java.util.stream.Collectors;
  * The class helps read data from xml and csv files.
  */
 public class ParserImpl implements Parser {
+
+    /**
+     * This is xml parser.
+     */
     private XmlParser xmlParser;
+
+    /**
+     * This is csv parser.
+     */
     private CsvParser csvParser;
+
+    /**
+     * This is logger.
+     */
     private static final Logger LOGGER = Logger.getLogger(ReportImpl.class);
 
+    /**
+     * This is constructor ParserImpl.
+     */
     public ParserImpl() {
         this.xmlParser = new XmlParser();
         this.csvParser = new CsvParser();
@@ -33,7 +48,8 @@ public class ParserImpl implements Parser {
      */
     @Override
     public final List<Request> readDataFromFiles(final String filePath,
-                                                 final boolean removeDuplicates) throws UnsupportedFileExtensionException {
+                                                 final boolean removeDuplicates)
+            throws UnsupportedFileExtensionException {
         List<Request> requestsEntities = new ArrayList<>();
         if (filePath != null) {
             if (filePath.toLowerCase().endsWith(".xml")) {
@@ -63,12 +79,14 @@ public class ParserImpl implements Parser {
      */
     @Override
     public final List<Request> readDataFromFiles(final List<String> filesPaths,
-                                                 final boolean removeDuplicates) throws UnsupportedFileExtensionException {
+                                                 final boolean removeDuplicates)
+            throws UnsupportedFileExtensionException {
         List<Request> requestsEntities = new ArrayList<>();
         List<Request> allRequestsEntities = new ArrayList<>();
         requestsEntities.addAll(
                 filesPaths.stream()
-                        .filter(p -> p != null && (p.endsWith(".xml") || p.endsWith(".csv")))
+                        .filter(p -> p != null
+                                && (p.endsWith(".xml") || p.endsWith(".csv")))
                         .map(p -> prepareRequests(p, removeDuplicates))
                         .flatMap(List::stream).collect(Collectors.toList())
         );
@@ -76,6 +94,14 @@ public class ParserImpl implements Parser {
         return allRequestsEntities;
     }
 
+    /**
+     * This metod helps prepare requests.
+     *
+     * @param p                is path to file
+     * @param removeDuplicates true if duplicate requests are to be removed,
+     *                         otherwise false
+     * @return list with objects extracted from files
+     */
     private List<Request> prepareRequests(final String p,
                                           final boolean removeDuplicates) {
         List<Request> requests = new ArrayList<>();
